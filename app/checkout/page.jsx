5ec94072d4b06506aa7194b5da8c71fb210386e0
erlanier1 <erica.lanier@unisonglobal.com>
2026-04-20 20:03:57 -0400

    fix error

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { loadStripe } from '@stripe/js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
